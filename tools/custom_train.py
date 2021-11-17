3146de79ee873e9afe2a6c4dad0ce07772bf8fbf
Nout Kleef <noutkleef1@gmail.com>
2021-11-17 17:52:17 +0000

    fix script

--- tools/custom_train.py
+++ tools/custom_train.py
@@ -2,6 +2,6 @@ import subprocess
 
 subprocess.call([
     "./tools/dist_train.sh",
-    "configs/diss/diss-nu-3d.py"
+    "configs/diss/diss-nus-3d.py",
     "1"                             # 1 GPU
 ])
