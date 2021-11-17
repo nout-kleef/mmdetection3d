d39f5a579dce4e7892ef0fb6cb5ec7f6260412ea
Nout Kleef <noutkleef1@gmail.com>
2021-11-17 18:06:47 +0000

    copy custom schedule and dataset file from pointpillars

--- configs/diss/diss-nus-3d.py
+++ configs/diss/diss-nus-3d.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_base_/models/diss_nus.py',         # ???
-    '../_base_/datasets/nus-3d.py',         # ingest the dataset
-    '../_base_/schedules/schedule_2x.py',   # define learning schedule
-    '../_base_/default_runtime.py'          # ???
+    '../_base_/models/diss_nus.py',             # ???
+    '../_base_/datasets/diss_nus-3d.py',        # ingest the dataset
+    '../_base_/schedules/schedule_diss.py',     # define learning schedule
+    '../_base_/default_runtime.py'              # ???
 ]
