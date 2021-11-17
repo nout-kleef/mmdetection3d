2316481489daacd26f07745c2540611fbcc979de
Nout Kleef <noutkleef1@gmail.com>
2021-11-17 17:32:57 +0000

    copy custom config from pointpillars

---
+++ configs/diss/diss-nus-3d.py
@@ -0,0 +1,6 @@
+_base_ = [
+    '../_base_/models/diss_nus.py',         # ???
+    '../_base_/datasets/nus-3d.py',         # ingest the dataset
+    '../_base_/schedules/schedule_2x.py',   # define learning schedule
+    '../_base_/default_runtime.py'          # ???
+]
