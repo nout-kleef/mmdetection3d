a924393abef60055cc1ed7b53c8c7b723d7aba24
Nout Kleef <noutkleef1@gmail.com>
2022-01-26 16:37:51 +0000

    add preprocessing scripts from Fangqiang

---
+++ preprocess/pcl_sync.py
@@ -0,0 +1,182 @@
+# sync and concat multi-radar data to lidar scans with pose compensation
+import csv
+import open3d
+import numpy as np
+from math import sin, cos, pi
+import os
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from glob import glob
+from os.path import join as osp
+import pandas as pd
+from pose_extract import get_trans_from_gnssimu, get_matrix_from_ext, get_interpolate_pose
+
+def get_cor_radar_idx(lidar_ts, radar_ts):
+    
+    f_cor = []
+    l_cor = []
+    r_cor = []
+    cor_idx = {"front_idx": [], "left_idx": [], "right_idx": []}
+    cor_ts = {"front_ts": [], "left_ts": [], "right_ts": []}
+    for target in lidar_ts:
+        for key in radar_ts:
+            diff = abs(radar_ts[key] - target)
+            idx = diff.argmin()
+            cor_idx[key[:-3]+"_idx"].append(idx)
+            cor_ts[key].append(radar_ts[key][idx])
+            
+    for key in cor_idx:
+        cor_idx[key]=np.array(cor_idx[key])
+    for key in cor_ts:
+        cor_ts[key]=np.array(cor_ts[key])
+   
+    return cor_idx,cor_ts
+
+def get_cor_gt_idx(lidar_ts, gt_ts):
+    
+    cor_idx = []
+    cor_ts = []
+    
+    for target in lidar_ts:
+        diff = abs(gt_ts - target)
+        idx = diff.argmin()
+        cor_idx.append(idx)
+        cor_ts.append(gt_ts[idx])
+    
+    cor_idx = np.array(cor_idx)
+    cor_ts = np.array(cor_ts)
+    
+    return cor_idx, cor_ts
+
+def radar_temp_comp(data, data_ts, lidar_ts, pose, pose_ts, radar_trans, target_trans):
+    
+    xyz = np.concatenate((data[:3],np.ones((1,len(data[0])))),axis=0)
+    src_diff = abs(pose_ts-data_ts)
+    src_idx = src_diff.argmin()
+    src_pose = pose[src_idx]
+    tgt_diff = abs(pose_ts-lidar_ts)
+    tgt_idx = tgt_diff.argmin()
+    tgt_pose = pose[tgt_idx]
+    # transform from radar to ego
+    ego_xyz = radar_trans @ xyz 
+    # transform from source to global
+    glb_xyz = src_pose @ ego_xyz
+    # tranform from global to target
+    tgt_xyz = np.linalg.inv(tgt_pose) @ glb_xyz
+    # transform from target timestamp to target sensor
+    front_xyz = np.linalg.inv(target_trans) @ tgt_xyz
+    # concate transformed xyz with other data
+    data_comp = np.concatenate((front_xyz[:3],data[3:]),axis=0)
+    
+    return data_comp
+    
+def concat_radars(base_ts, front_files,left_files,right_files,cor_idx,cor_ts,lidar_ts, ego_pose,pose_ts,sensor_T,save_path):
+    
+    
+    data_len = len(cor_ts["front_ts"])
+    for i in tqdm(range(data_len)):
+        f_data = pd.read_table(front_files[cor_idx["front_idx"][i]], sep=",", header=None).values[1:,1:].T.astype(np.float32)
+        #l_data = pd.read_table(left_files[cor_idx["left_idx"][i]], sep=",", header=None).values[1:,1:].T.astype(np.float32)
+        #r_data = pd.read_table(right_files[cor_idx["right_idx"][i]], sep=",", header=None).values[1:,1:].T.astype(np.float32)
+        f_comp = radar_temp_comp(f_data, cor_ts["front_ts"][i], lidar_ts[i], ego_pose,pose_ts, sensor_T['radar_front'],sensor_T['radar_front'])
+        #l_comp = radar_temp_comp(l_data, cor_ts["left_ts"][i], lidar_ts[i], ego_pose,pose_ts, sensor_T['radar_left'],sensor_T['radar_front'])
+        #r_comp = radar_temp_comp(r_data, cor_ts["right_ts"][i], lidar_ts[i], ego_pose,pose_ts, sensor_T['radar_right'],sensor_T['radar_front'])
+        #concat_comp = np.concatenate((f_comp,l_comp,r_comp),axis=1)
+        concat_comp = f_comp
+        path = os.path.join(save_path, str(int(lidar_ts[i]*1e3)+base_ts).zfill(13) + ".csv")
+        data = pd.DataFrame(concat_comp.T)
+        data.to_csv(path)
+
+def save_sync_gt(save_gt,gt_cor_idx,base_ts,lidar_ts,gt_files):
+    
+    data_len = len(lidar_ts)
+    for i in tqdm(range(data_len)):
+        ts = lidar_ts[i]
+        gt_ts = int(ts*1e3+base_ts)
+        gt_obj = np.loadtxt(gt_files[gt_cor_idx[i]])
+        gt_fname = str(gt_ts).zfill(13) + ".csv"
+        full_fname = osp(save_gt, gt_fname)
+        np.savetxt(full_fname, gt_obj, fmt="%s")        
+
+def main():
+    
+    # save path of concatenated radar point clouds
+    save_path = "../20220118/sync_radar/"
+    # save path of sync gt filees
+    save_gt = "../20220118/sync_gt"
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+    if not os.path.exists(save_gt):
+        os.mkdir(save_gt)
+    # initialize the lidar and radar timestamps and files
+    radar_path = {'front': '../20220118/radar_front/', 
+                  'left': '../20220118/radar_front/',
+                  'right': '../20220118/radar_front/'
+                  }
+    lidar_path = "../20220118/lidar/20220118-13-43-20_C/"
+    pose_path = "../20220118/gnssimu-sample-v6@2.csv"
+    gt_path = "../20220118/gt_abs/"
+    
+    base_ts = 1642484600284 ## utc base timestamp, for lidar and robosense
+    base_ts_local = 1642484600826 ## local base timestamp, for radar and pose
+
+    front_files = sorted(glob(radar_path["front"]+'*.csv'))
+    left_files = sorted(glob(radar_path["left"]+'*.csv'))
+    right_files = sorted(glob(radar_path["right"]+'*.csv'))
+    lidar_files = sorted(glob(lidar_path+'*.pcd')) 
+    gt_files = sorted(glob(gt_path+'*.csv'))
+    
+    radar_ts = {'front_ts': [], "left_ts" : [], "right_ts": []}
+    lidar_ts = []
+    gt_ts = []
+    gt_bias = 18000 + 100 + 25
+    
+    
+    for file in front_files:
+        radar_ts["front_ts"].append((float(file.split('/')[-1].split('.')[0])-base_ts_local)/1e3)
+    for file in left_files:
+        radar_ts["left_ts"].append((float(file.split('/')[-1].split('.')[0])-base_ts_local)/1e3)
+    for file in right_files:
+        radar_ts["right_ts"].append((float(file.split('/')[-1].split('.')[0])-base_ts_local)/1e3)
+    for file in lidar_files:
+        lidar_ts.append((float(file.split('/')[-1].split('.')[0])-base_ts)/1e3)
+    for file in gt_files:
+        gt_ts.append((float(file.split('/')[-1].split('.')[0])-base_ts-gt_bias)/1e3)
+    
+    gt_ts = np.array(gt_ts)
+        
+    for key in radar_ts:
+        radar_ts[key] = np.array(radar_ts[key])
+    
+    # get transformation matrix of sensors from extrinsics
+    radar_front_ext = np.array([0.06, -0.2, 0.7, -3.5, 2, 180])
+    radar_left_ext = np.array([0.06, -0.2, 0.7, -3.5, 2, 180])
+    radar_right_ext = np.array([0.06, -0.2, 0.7, -3.5, 2, 180])
+    lidar_ext = np.array([0, 0, -0.3, -2.5, 0, 0])
+    
+    radar_front_T = get_matrix_from_ext(radar_front_ext)
+    radar_left_T = get_matrix_from_ext(radar_left_ext)
+    radar_right_T = get_matrix_from_ext(radar_right_ext)
+    lidar_T = get_matrix_from_ext(lidar_ext)
+    
+    sensor_T = {"radar_front": radar_front_T, "radar_left": radar_left_T,
+                "radar_right": radar_right_T, "lidar": lidar_T}
+    
+    # get the high-frenquency vehicle pose data (100Hz)
+    ego_pose, pose_ts = get_interpolate_pose(pose_path,scale=10)
+    pose_ts = (pose_ts-base_ts_local)/1e3
+    # get the corresponding timestamp (sync with lidar) of gt
+    gt_cor_idx, gt_cor_ts = get_cor_gt_idx(lidar_ts, gt_ts)
+    # get the corresponding timestamp (sync with lidar) of each radar sensor 
+    cor_idx, cor_ts = get_cor_radar_idx(lidar_ts, radar_ts)
+    
+    # concat multi-radar data with temporal compensation using pose data
+    concat_radars(base_ts, front_files,left_files,right_files,cor_idx,cor_ts, lidar_ts, ego_pose,pose_ts,sensor_T,save_path)
+    # save sync gt to new folder, no need pose data, just to match gt and lidar
+    save_sync_gt(save_gt,gt_cor_idx,base_ts,lidar_ts,gt_files)
+    
+
+       
+    
+if __name__ == '__main__':
+    main()
---
+++ preprocess/vis_pcl.py
@@ -0,0 +1,125 @@
+from argparse import RawDescriptionHelpFormatter
+import open3d as o3d
+import numpy as np
+from glob import glob
+from scipy.spatial.transform import Rotation as R
+import os
+import pandas as pd
+
+
+# lidar_ext = [-2.502, -0.004, 2.033, 3.5, -0.2, 0 ]
+lidar_ext = [0, 0, -0.3, -2.5, 0, 0]
+radar_ext = [0.06, -0.2, 0.7, -3.5, 2, 180]
+
+def csv2geometry(fname):
+    pts = pd.read_table(fname,sep=",", header=None).values[1:,1:4]
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(pts)
+    return pcd
+
+def get_matrix_from_ext(ext):
+    rot = R.from_euler('ZYX', ext[3:], degrees=True)
+    rot_m = rot.as_matrix()
+    x, y, z = ext[:3]
+    tr = np.eye(4)
+    tr[:3,:3] = rot_m
+    tr[:3, 3] = np.array([x, y, z]).T
+    return tr
+
+def get_rotation(arr):
+    x,y,_ = arr
+    yaw = np.arctan(y/x)
+    angle = np.array([0, 0, yaw])
+    r = R.from_euler('XYZ', angle)
+    return r.as_matrix()
+
+def get_bbx_param(obj_info):
+    center = obj_info[2:5] #+ np.array([-2.5, 0, 0])
+    
+    extent = obj_info[5:8]
+    
+    
+    angle = obj_info[8:-1]
+    # angle[0] = -angle[0]
+    rot_m = get_rotation(angle)
+    # rot_m = np.eye(3)
+    
+    obbx = o3d.geometry.OrientedBoundingBox(center.T, rot_m, extent.T)
+    return obbx
+
+root_path = "../20220118/"
+
+lidar_files = sorted(glob(root_path + "lidar/20220118-13-43-20_C/*.pcd"))
+
+gt_files = sorted(glob(root_path + "sync_gt/*.csv"))
+
+radar_files = sorted(glob(root_path + "sync_radar/*.csv"))
+
+img_path = root_path + "img_vis_all/"
+
+save_img = True
+if save_img:
+    if not os.path.exists(img_path):
+        os.mkdir(img_path)
+    else:
+        os.system("rm -r " + img_path)
+        os.mkdir(img_path)
+
+
+start = 30
+gt_fname = gt_files[4]
+lidar_tr = get_matrix_from_ext(lidar_ext)
+radar_tr = get_matrix_from_ext(radar_ext)
+lidar_pcd = o3d.io.read_point_cloud(lidar_files[3])
+lidar_pcd.transform(lidar_tr)
+lidar_pcd.paint_uniform_color([1, 0, 0])
+
+radar_pcd = o3d.geometry.PointCloud()
+radar_temp_pcd = csv2geometry(radar_files[0])
+radar_pcd.points = radar_temp_pcd.points
+radar_pcd.paint_uniform_color([0, 0, 1])
+radar_pcd.transform(radar_tr)
+
+
+vis = o3d.visualization.Visualizer()
+vis.create_window(width=1920,height=1080)
+vis.add_geometry(lidar_pcd)
+vis.add_geometry(radar_pcd)
+
+# print(gt_fname)
+gt_data = np.loadtxt(gt_fname)
+box_list = []
+for obj_info in gt_data:
+    obj_bbx = get_bbx_param(obj_info)
+    box_list += [obj_bbx]
+    vis.add_geometry(obj_bbx)
+
+for idx in range(start, len(lidar_files)):
+    temp_pcd = o3d.io.read_point_cloud(lidar_files[idx])
+    lidar_pcd.points = temp_pcd.points
+    lidar_pcd.paint_uniform_color([0, 0, 1])
+    lidar_pcd.transform(lidar_tr)
+
+    temp_pcd = csv2geometry(radar_files[idx])
+    radar_pcd.points = temp_pcd.points
+    radar_pcd.paint_uniform_color([1, 0, 0])
+    radar_pcd.transform(radar_tr)
+
+    vis.update_geometry(lidar_pcd)
+    vis.update_geometry(radar_pcd)
+    for box in box_list:
+        vis.remove_geometry(box, reset_bounding_box=False)
+    box_list = []
+    gt_data = np.loadtxt(gt_files[idx])
+    for obj_info in gt_data:
+        obj_bbx = get_bbx_param(obj_info)
+        box_list += [obj_bbx]
+        vis.add_geometry(obj_bbx, reset_bounding_box=False)
+    
+    vis.poll_events()
+    vis.update_renderer()
+    if save_img:
+        fname = os.path.join(img_path, str(idx).zfill(9) + '.png')
+        vis.capture_screen_image(fname)
+
+
