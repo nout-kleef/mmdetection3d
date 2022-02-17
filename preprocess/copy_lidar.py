c08afe774f104f952702cbeb7c47a31272c11fa8
Nout Kleef <noutkleef1@gmail.com>
2022-02-17 15:29:58 +0000

    save storage space by using symlinks for lidar instead of copying the files

--- preprocess/copy_lidar.py
+++ preprocess/copy_lidar.py
@@ -1,9 +1,10 @@
 import os
 import glob
-import shutil
 import mmcv
+from pathlib import Path
 
 def copy_lidar(load_dir, save_dir):
+    # NB: not actually copying files, but creating symlinks to save space
     # figure out the source directory
     lidar_source = os.path.join(load_dir, 'input', 'lidar')
     path_options = os.listdir(lidar_source)
@@ -12,8 +13,12 @@ def copy_lidar(load_dir, save_dir):
     lidar_source = os.path.join(lidar_source, path_options[0])
     # copy source files to processed directory to allow for easier downstream processing
     lidar_filenames = os.path.join(lidar_source, '*.pcd')
-    destination = os.path.join(save_dir, 'inhouse_format', 'lidar')
-    if not os.path.exists(destination):
-        os.mkdir(destination)
-    for _, filename in enumerate(mmcv.track_iter_progress(glob.glob(lidar_filenames))):
-        shutil.copy(filename, destination)
+    dest_dir = os.path.join(save_dir, 'inhouse_format', 'lidar')
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    for _, src_path in enumerate(mmcv.track_iter_progress(glob.glob(lidar_filenames))):
+        dest_path = os.path.join(dest_dir, Path(src_path).name)
+        if os.path.exists(dest_path):
+            os.remove(dest_path)
+        os.symlink(src_path, dest_path)
+        
--- preprocess/preprocess.py
+++ preprocess/preprocess.py
@@ -13,7 +13,6 @@ def main():
     parser = argparse.ArgumentParser(description='Read raw radar sweeps')
     parser.add_argument('load_dir', help='path to root of directory containing unprocessed data')
     parser.add_argument('save_dir', help='path to root of directory to store processed data')
-    parser.add_argument('--delete_existing', action='store_true', help='if preprocessed data already exists, delete prior to new run')
     parser.add_argument('--skip_read_raw', action='store_true', help='skip read_raw.py')
     parser.add_argument('--skip_extract_gt', action='store_true', help='skip extract_gt.py')
     parser.add_argument('--skip_pcl_sync', action='store_true', help='skip pcl_sync.py')
@@ -26,15 +25,6 @@ def main():
         os.path.join(args.save_dir, 'inhouse_format', 'radar_raw'),
         os.path.join(args.save_dir, 'inhouse_format', 'gt_raw'),
     ]
-    _processed_paths = [
-        os.path.join(args.save_dir, 'inhouse_format', 'gt'),
-        os.path.join(args.save_dir, 'inhouse_format', 'radar'),
-        # os.path.join(args.save_dir, 'inhouse_format', 'lidar'),
-    ]
-
-    if args.delete_existing:
-        print('*** STEP 0 - delete existing data ***')
-        _purge(_raw_paths + _processed_paths)
 
     print('*** STEP 1 - read_raw.py ***')
     if not args.skip_read_raw: read_raw.read_raw(args.load_dir, args.save_dir)
