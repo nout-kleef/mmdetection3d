f1879719623778169de9e7fe9f463f5b33f9d603
Nout Kleef <noutkleef1@gmail.com>
2022-02-25 10:53:59 +0000

    fix lidar visualisation

--- tools/visualise.py
+++ tools/visualise.py
@@ -22,17 +22,18 @@ def load_lidar_pointcloud(file):
     pcd.paint_uniform_color([0, 0, 1])
     return pcd  # todo: transform
 
-def visualise(vis, lidar, curr_lidar_pc=None, curr_radar_pc=None):
-    lidar_pc = load_lidar_pointcloud(lidar)
+def visualise(dir, ts):
+    vis = open3d.visualization.VisualizerWithEditing()
+    vis.create_window(width=1920,height=1080)
+    lidar_file = os.path.join(dir, 'lidar', f'{ts}.bin')
+    lidar_pc = load_lidar_pointcloud(lidar_file)
+    lidar_pc.paint_uniform_color([0, 0, 1])
     # lidar_pc.transform(lidar_tr)
-    if curr_lidar_pc:
-        curr_lidar_pc.points = lidar_pc.points
-        curr_lidar_pc.paint_uniform_color([0, 0, 1])
-        vis.update_geometry(curr_lidar_pc)
-    else:
-        vis.add_geometry(lidar_pc)
-        curr_lidar_pc = lidar_pc
-    return curr_lidar_pc, None
+    vis.add_geometry(lidar_pc)
+    # display
+    # vis.poll_events()
+    # vis.update_renderer()
+    vis.run()
 
 def main():
     parser = argparse.ArgumentParser(description='Visualise lidar + radar pointclouds')
@@ -40,15 +41,8 @@ def main():
     parser.add_argument('--save_dir', help='Directory used to save images (optional)')
     parser.add_argument('timestamps', nargs='*')
     args = parser.parse_args()
-    
-    print(args.load_dir)
-    print(args.save_dir)
-    print(args.timestamps)
-    save_img = args.save_dir is not None
 
-    lidar_files = sorted(glob.glob(os.path.join(args.load_dir, "lidar", "*.bin")))
-    # gt_files = sorted(glob.glob(os.path.join(load_dir, "gt", "*.csv")))
-    radar_files = sorted(glob.glob(os.path.join(args.load_dir, "radar", "*.bin")))
+    save_img = args.save_dir is not None
     if save_img:
         if not os.path.exists(args.save_dir):
             os.mkdir(args.save_dir)
@@ -56,38 +50,15 @@ def main():
             os.system("rm -r " + args.save_dir)
             os.mkdir(args.save_dir)
 
-    start = 30
-    # gt_fname = gt_files[4]
     lidar_tr = get_matrix_from_ext(LIDAR_EXT)
     radar_tr = get_matrix_from_ext(RADAR_EXT)
-    vis = open3d.visualization.Visualizer()
-    vis.create_window(width=1920,height=1080)
-
-    lidar_pc, radar_pc = visualise(vis, lidar_files[0], radar_files[0])
-
-    # print(gt_fname)
-    # gt_data = np.loadtxt(gt_fname)
-    # box_list = []
-    # for obj_info in gt_data:
-    #     obj_bbx = get_bbx_param(obj_info)
-    #     box_list += [obj_bbx]
-    #     vis.add_geometry(obj_bbx)
-
-    for idx in range(start, len(lidar_files)):
-        lidar_pc, radar_pc = visualise(vis, lidar_files[idx], lidar_pc, radar_pc)
-        # for box in box_list:
-        #     vis.remove_geometry(box, reset_bounding_box=False)
-        # box_list = []
-        # gt_data = np.loadtxt(gt_files[idx])
-        # for obj_info in gt_data:
-        #     obj_bbx = get_bbx_param(obj_info)
-        #     box_list += [obj_bbx]
-        #     vis.add_geometry(obj_bbx, reset_bounding_box=False)
-        vis.poll_events()
-        vis.update_renderer()
+    
+    for ts in args.timestamps:
+        visualise(args.load_dir, ts)
+        
         if save_img:
-            fname = os.path.join(args.save_dir, f'{idx}.png')
-            vis.capture_screen_image(fname)
+            fname = os.path.join(args.save_dir, f'{ts}.png')
+            # vis.capture_screen_image(fname)
 
 def get_matrix_from_ext(ext):
     rot = R.from_euler('ZYX', ext[3:], degrees=True)
