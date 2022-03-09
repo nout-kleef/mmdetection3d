1b016e36769614de7b15de6f369a42b5a6d98c8c
Nout Kleef <noutkleef1@gmail.com>
2022-03-09 00:08:32 +0000

    launch radar-only experiment

--- configs/dissertation/inhouse_radar.py
+++ configs/dissertation/inhouse_radar.py
@@ -5,8 +5,6 @@ data_root = 'data/inhouse_radar_only/kitti_format/'
 
 # Experiment settings
 runner = dict(max_epochs=160)
-evaluation = dict(interval=1)
+evaluation = dict(interval=4)
 
-data = dict(
-        samples_per_gpu=11,
-        workers_per_gpu=4)
+data = dict(samples_per_gpu=11, workers_per_gpu=4)
