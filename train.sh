aa47ed32406687844d260dc61ba5c602cda215f9
Nout Kleef <noutkleef1@gmail.com>
2022-03-14 23:53:21 +0000

    remove sleep

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 3
-echo "launching."
+echo "flag branch"
+git branch
 
 CUDA_VISIBLE_DEVICES=0 python tools/train.py configs/dissertation/debug/nC_nT_nP.py --work-dir /mnt/12T/nout/archive/debug/nC_nT_nP
