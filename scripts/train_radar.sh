fa4aa54805dd7f97c7c0e177b7e67e9617c4c12b
Nout Kleef <noutkleef1@gmail.com>
2022-03-21 13:55:20 +0000

    restart radar experiments after shutdown

--- scripts/train_radar.sh
+++ scripts/train_radar.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=1 python tools/train.py \
 /mnt/12T/nout/mmdetection3d/configs/dissertation/inhouse_radar.py \
 --work-dir /mnt/12T/nout/archive/V2/radar_filtered \
-# --resume-from X
+--resume-from /mnt/12T/nout/archive/V2/radar_filtered/epoch_2.pth
